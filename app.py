a391f794ead71d0a2c6c3158e569263218d1f6a7
joji <rainbreeze2@naver.com>
2025-10-01 16:54:17 +0900

    add cors

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# 허용할 출처 목록 (예: 로컬호스트, 특정 도메인)
+origins = [
+    "*",  # 테스트용으로 모든 도메인 허용 (실서비스 시엔 제한 권장)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] to allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/hello")
 def say_hello():
